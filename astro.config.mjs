aca9d291065e9ec37d5cf3ed0f47f77acca8d15d
Wei Yang <wei.yang@rwth-aachen.de>
2023-12-05 13:27:40 +0100

    Add preact island component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3000, host: true },
+  server: {
+    port: 3000,
+    host: true
+  },
   site: "https://astro-tutorial-wolfboyyang.netlify.app/",
+  integrations: [preact()]
 });
