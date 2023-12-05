2fe5d833651706f6a915cc01c1933385f63fe11f
Wei Yang <wei.yang@rwth-aachen.de>
2023-12-05 19:58:14 +0100

    comment server config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true
-  },
+  // server: {
+  //   port: 3000,
+  //   host: true
+  // },
   site: "https://astro-tutorial-wolfboyyang.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
 });
